5dd6a60552f6258d4e77869309fed1b2841ffd48
Overdrowsed <llomidze5@gmail.com>
2022-07-20 16:37:01 +0400

    Update Figure.java

--- Homework/XXI/src/figure/Figure.java
+++ Homework/XXI/src/figure/Figure.java
@@ -1,33 +1,33 @@
 package figure;
 /**
- * Abstract {@code Figure} class that represent a blueprint for shapes.
+ * {@code Figure} class that represent a blueprint for shapes.
  * Contains methods that the shapes should inherit.
  * 
  * @version 1.0
  * @author Luka Lomidze
  */
-public abstract class Figure implements Comparable<Figure>{
+public class Figure implements Comparable<Figure>{
 
     /**
      * A getter method for the perimeter of the shape.
      * 
      * @return perimeter of the shape
      */
-    public abstract double getLength();
+    public abstract double getLength() { return -1;}
 
     /**
      * A getter method for the area of the shape.
      * 
      * @return area of the shape
      */
-    public abstract double getArea();
+    public abstract double getArea(){ return -1;}
 
     /**
      * Validates that the shape has a valid configuration.
      * 
      * @return {@code true} if the shape has a valid configuration, {@code false} otherwise
      */
-    public abstract boolean validate();
+    public abstract boolean validate(){ return -1;}
 
     @Override
     public int compareTo(Figure other) {
